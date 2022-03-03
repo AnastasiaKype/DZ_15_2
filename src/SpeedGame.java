1cc42003dd70d1e76f97eef894dea84bca17b663
AnastasiaKype <black_cat123@mail.ru>
2022-03-03 19:12:22 +0300

    start

---
+++ src/SpeedGame.java
@@ -0,0 +1,29 @@
+public class SpeedGame {
+
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+
+        int numbersOfPlayers = 0;
+
+        if (isGreenLight == true) {
+
+            System.out.println("Выбывших: " + numbersOfPlayers);
+        } else {
+            if (speedOfPlayer1 > 0) {
+                numbersOfPlayers = numbersOfPlayers + 1;
+            }
+            if (speedOfPlayer2 > 0) {
+                numbersOfPlayers = numbersOfPlayers + 1;
+            }
+            if (speedOfPlayer3 > 0) {
+                numbersOfPlayers = numbersOfPlayers + 1;
+            }
+            System.out.println("Выбывших: " + numbersOfPlayers);
+        }
+    }
+    
+}
